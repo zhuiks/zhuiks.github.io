39714334003547c03999fb9c3f7a628ce3bd0ceb
zhuiks <zhuke@ukr.net>
2020-10-27 10:53:04 -0500

    pageable component

--- components/pips.tsx
+++ components/pips.tsx
@@ -2,22 +2,29 @@ import React from 'react'
 import styles from './pips.module.scss'
 
 interface PipsProps {
-  links: string[]
+  links: string[] | {[id: string]: string}
   active?: string | number
   onClick?: () => void
 }
+const PipItem: React.FC<{id: string, title?: string, active?: boolean}> = ({id, title, active}) => (
+  <li key={id}>
+  <a 
+    className={active ? styles.active : styles.pip } 
+    href={`#${id}`}
+    title={title}
+  >
+    <span />
+  </a>
+</li>
+
+)
 const Pips: React.FC<PipsProps> = ({ links,  active}) => (
   <nav className={styles.pips}>
     <ul>
-      {links.map((r, i) => (
-        <li key={r}>
-          <a 
-            className={typeof active === "number" && active === i || active === r ? styles.active : styles.pip } 
-            href={`#${r}`}
-          >
-            <span />
-          </a>
-        </li>
+      {Array.isArray(links) ? links.map((r, i) => (
+        <PipItem key={i} id={r} active={typeof active === "number" && active === i || active === r} />
+      )) : Object.entries(links).map(([id, title], i) => (
+        <PipItem key={i} id={id} title={title} active={typeof active === "number" && active === i || active === id} />
       ))}
     </ul>
   </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/spa.module.scss'
 import Section from '../components/section';
 import { Data, getData } from '../lib/data';
 import { GetStaticProps } from 'next';
-import Pips from '../components/pips'
+import Pageable from '../components/pageable'
 
 export const getStaticProps: GetStaticProps = async () => {
   const data = getData()
@@ -20,11 +20,6 @@ export const getStaticProps: GetStaticProps = async () => {
 const SPA: React.FC<{ data: Data }> = ({ data }) => {
   const title = "Evgen Kucherov"
   const tagLine = "IT Consulting / Web Development"
-  const arrData = Object.entries(data)
-  const ids = Object.keys(data)
-  // React.useEffect(() => {
-  //   new Pageable('main', {freeScroll: true})
-  // }, [])
   return (
     <>
       <Head>
@@ -33,12 +28,11 @@ const SPA: React.FC<{ data: Data }> = ({ data }) => {
         <link href="https://fonts.googleapis.com/css2?family=Lora:wght@700&family=Poiret+One&family=Roboto&display=swap" rel="stylesheet" />
       </Head>
 
-      <main className={styles.main}>
-        {arrData.map(([id, section], i) => (
+      <Pageable>
+        {Object.entries(data).map(([id, section], i) => (
           <Section key={i} name={id} title={section.title} tag={section.tag} details={section.details} quote={section.quote} colors={section.colors} />
         ))}
-      </main>
-      <Pips links={ids} active="code"/>    
+      </Pageable>
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
