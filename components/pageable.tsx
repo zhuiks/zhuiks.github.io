b47f88f7adb485a599c98e829deb5ae9cd2ac00d
zhuiks <zhuke@ukr.net>
2020-10-27 16:27:40 -0500

    refactor scroll animation

--- components/pageable.tsx
+++ components/pageable.tsx
@@ -6,6 +6,8 @@ const DELTA = 10
 interface PageableProps {
 
 }
+let justScrolled = false
+
 const Pageable: React.FC<PageableProps> = ({ children }) => {
   const [index, setIndex] = useState(0)
   const [shift, setShift] = useState(0)
@@ -23,24 +25,27 @@ const Pageable: React.FC<PageableProps> = ({ children }) => {
   if(Math.abs(shift) > 30 && !(shift < 0 && index === 0 ) && !(index === Object.keys(links).length-1 && shift > 0)) {
     setIndex(shift < 0 ? index -1 : index + 1)
     setShift(0)
+    justScrolled = true
+    setTimeout(() => {
+      justScrolled = false
+    }, 1000)
   }
   const pixelShift = index * height + shift * height / 100
   console.log(`--- Render: pixelShift=${pixelShift}`)
 
   const handleWheel = (event: WheelEvent<HTMLElement>) => {
-    console.log(`Wheeeel: ${event.deltaY}`)
-    if(event.deltaY < 0 && index === 0 && shift - DELTA < 0) return
+    if(justScrolled || event.deltaY < 0 && index === 0 && shift - DELTA < 0) return
     setShift(event.deltaY < 0 ? shift - DELTA : shift + DELTA)
   }
   return (
     <>
-      <main ref={mainRef} onWheel={handleWheel}>{children}</main>
+      <main ref={mainRef} onWheel={handleWheel} style={{marginTop: -pixelShift}}>{children}</main>
       <Pips links={links} active={index} />
       <style jsx>{`
         main {
           min-height: 100vh;
           width: 100%;
-          margin-top: -${pixelShift}px;
+          transition: margin-top 0.3s ease-out
         }
       `}</style>
     </>
